468a5d64b34bd9a3e5b701569690775dd1af5d9f
c-c-k <c-c-k@nym.hush.com>
2026-06-16 21:21:49 +0300

    chore(tests): temporarily disable `libuv` tests when oximlua

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,6 +1,12 @@
 mod api;
 mod r#macro;
 
-// Libuv bindings don't work on Windows.
-#[cfg(not(any(target_os = "windows", target_env = "msvc")))]
+// // Libuv bindings don't work on Windows.
+// #[cfg(not(any(target_os = "windows", target_env = "msvc")))]
+#[cfg(not(any(
+    // Libuv bindings don't work on Windows.
+    target_os = "windows", target_env = "msvc",
+    // libuv not implemented for oximlua yet
+    feature = "oximlua"
+)))]
 mod libuv;
