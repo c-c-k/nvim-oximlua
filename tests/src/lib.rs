1ebca36320e4afbde99e840b12362fe76a4c25ad
c-c-k <c-c-k@nym.hush.com>
2026-07-25 17:22:28 +0300

    chore(tests): restore `libuv` tests when oximlua

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,12 +1,6 @@
 mod api;
 mod r#macro;
 
-// // Libuv bindings don't work on Windows.
-// #[cfg(not(any(target_os = "windows", target_env = "msvc")))]
-#[cfg(not(any(
-    // Libuv bindings don't work on Windows.
-    target_os = "windows", target_env = "msvc",
-    // libuv not implemented for oximlua yet
-    feature = "oximlua"
-)))]
+// Libuv bindings don't work on Windows.
+#[cfg(not(any(target_os = "windows", target_env = "msvc")))]
 mod libuv;
